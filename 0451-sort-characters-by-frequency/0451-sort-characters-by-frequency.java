646d9a335524dd6c4da965cd7356a86e6b1328ef
Thershna <thershnat@gmail.com>
2026-08-16 21:17:23 +0530

    Time: 18 ms (21.3%), Space: 46.5 MB (78.34%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -5,17 +5,19 @@ class Solution {
             map.put(c,map.getOrDefault(c,0)+1);
         }
         PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
-        pq.addAll(map.keySet());
-        StringBuilder ans=new StringBuilder();
+        for(char c:map.keySet()){
+            pq.add(c);
+        }
+        StringBuilder sb=new StringBuilder();
         while(!pq.isEmpty()){
-            char ch=pq.poll();
-            int freq=map.get(ch);
-            for(int i=0;i<freq;i++){
-                ans.append(ch);
+            char c=pq.poll();
+            for(int i=0;i<map.get(c);i++){
+                sb.append(c);
             }
         }
-        return ans.toString();
-        
+        return sb.toString();
+
+
         
     }
 }
