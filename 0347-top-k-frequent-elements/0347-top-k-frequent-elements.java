c084498b5762324713051289b72a8f881a56afb9
Thershna <thershnat@gmail.com>
2026-08-16 20:08:21 +0530

    Time: 16 ms (37.89%), Space: 47.9 MB (20.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,32 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int x: nums){
+            map.put(x,map.getOrDefault(x,0)+1);
         }
+       PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
+       for(int x: map.keySet()){
 
-        PriorityQueue<Integer> pq =
-            new PriorityQueue<>(
-                (a, b) -> map.get(a) - map.get(b)
-            );
-
-        for (int n : map.keySet()) {
-
-            pq.add(n);
-
-            if (pq.size() > k) {
-                pq.poll();
-            }
+    
+        pq.add(x);
+        if(pq.size()>k){
+            pq.poll();
         }
-
-        int[] ans = new int[k];
-
-        for (int i = k - 1; i >= 0; i--) {
-            ans[i] = pq.poll();
-        }
-
-        return ans;
+       }
+       int[] arr=new int[k];
+       for(int i=k-1;i>=0;i--){
+        arr[i]=pq.poll();
+       }
+       return arr;
+        
     }
 }
